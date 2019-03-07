d4e97b702159cff0d5ef6012fb01ebd4f4be82ac
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-03-07 12:52:22 -0700

    Add feature to reset email through email link

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,16 @@ Rails.application.routes.draw do
 
   get '/', to: 'welcome#show'
 
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
   get '/profile', to: 'users#show'
-  get 'profile/edit', to: 'users#edit'
-  post 'profile/edit', to: 'users#update'
+  get '/profile/edit', to: 'users#edit'
+  post '/profile/edit', to: 'users#update'
+
+  get '/reset_form', to: 'users#reset_form'
+  post '/reset_form', to: 'users#password_reset'
 
   get '/registration', to: 'users#new'
   post '/registration', to: 'users#create'
@@ -27,8 +30,6 @@ Rails.application.routes.draw do
   get '/email_verification', to: 'email_verification#show'
   get '/password_reset', to: 'password_reset#show'
   post '/password_reset', to: 'password_reset#reset'
-  get '/reset_form', to: 'users#password_reset'
-  post '/reset_form', to: 'users#update'
 
   resources :topics do
     resources :bookmarks
